fe4d75aeb691af7099809fc382092c366008a635
TogoFire <togofire@mailfence.com>
2024-11-15 14:34:15 -0300

    Revert "Powerhub: Remove UDFPS switch bar"
    This reverts commit 43167427180ab63b6ee424beea73b8d3ac5847fd.

--- src/com/power/hub/fragments/lockscreen/UdfpsAnimation.java
+++ src/com/power/hub/fragments/lockscreen/UdfpsAnimation.java
@@ -24,12 +24,20 @@ import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -48,10 +56,22 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.widget.SettingsMainSwitchBar;
+import com.android.settingslib.search.Indexable;
+import com.android.settingslib.widget.OnMainSwitchChangeListener;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class UdfpsAnimation extends SettingsPreferenceFragment {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class UdfpsAnimation extends SettingsPreferenceFragment implements
+        OnMainSwitchChangeListener {
+
+    private Switch mSwitch;
 
     private RecyclerView mRecyclerView;
     private String mPkg = "com.power.hub.udfps.animations";
@@ -63,6 +83,7 @@ public class UdfpsAnimation extends SettingsPreferenceFragment {
     private String[] mAnimPreviews;
     private String[] mTitles;
 
+    private boolean mEnabled;
     private UdfpsAnimAdapter mUdfpsAnimAdapter;
 
     @Override
@@ -99,14 +120,49 @@ public class UdfpsAnimation extends SettingsPreferenceFragment {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3);
         mRecyclerView.setLayoutManager(gridLayoutManager);
         mUdfpsAnimAdapter = new UdfpsAnimAdapter(getActivity());
-        mRecyclerView.setAdapter(mUdfpsAnimAdapter);
 
         return view;
     }
 
     @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        final SettingsActivity activity = (SettingsActivity) getActivity();
+        final SettingsMainSwitchBar switchBar = activity.getSwitchBar();
+        mSwitch = switchBar.getSwitch();
+        mEnabled = Settings.System.getInt(getActivity().getContentResolver(),
+                       Settings.System.UDFPS_ANIM, 0) == 1;
+        mSwitch.setChecked(mEnabled);
+        setEnabled(mEnabled);
+        switchBar.setTitle(getActivity().getString(R.string.enable));
+        switchBar.addOnSwitchChangeListener(this);
+        switchBar.show();
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.UDFPS_ANIM, isChecked ? 1 : 0);
+        mSwitch.setChecked(isChecked);
+        setEnabled(isChecked);
+    }
+
+    public void setEnabled(boolean enabled) {
+        if (enabled) {
+            mRecyclerView.setAdapter(mUdfpsAnimAdapter);
+        } else {
+            mRecyclerView.setAdapter(null);
+        }
+    }
+
+    @Override
     public int getMetricsCategory() {
         return MetricsEvent.VOLTAGE;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 
     public class UdfpsAnimAdapter extends RecyclerView.Adapter<UdfpsAnimAdapter.UdfpsAnimViewHolder> {
